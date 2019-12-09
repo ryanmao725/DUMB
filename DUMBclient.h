c31b7f7cbd7d3b4e56e65f123e3792a633d81d1e
Shivan Modha <shivanmodha@gmail.com>
2019-12-09 18:55:43 -0500

    A birdy once told me I needed a head

---
+++ DUMBclient.h
@@ -0,0 +1,11 @@
+/**
+ * @author Shivan/Ryan
+ * Copyright 2019 Shivan/Ryan
+ */
+#ifndef DUMBCLIENT_H_
+#define DUMBCLIENT_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif  // DUMBCLIENT_H_
---
+++ DUMBserver.h
@@ -0,0 +1,11 @@
+/**
+ * @author Shivan/Ryan
+ * Copyright 2019 Shivan/Ryan
+ */
+#ifndef DUMBSERVER_H_
+#define DUMBSERVER_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif  // DUMBSERVER_H_
