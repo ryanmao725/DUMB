fdae9f66195867066d77330dc81d68450effaec8
ryanmao725 <ryan.mao.725@gmail.com>
2019-12-09 19:05:52 -0500

    Added error checking for DUMBclient

--- DUMBclient.c
+++ DUMBclient.c
@@ -1,8 +1,12 @@
-#include<unistd.h>
-#include<stdlib.h>
-#include<stdio.h>
+#include"DUMBclient.h"
 
 int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Error: Need to provide the IP-address and Port Number as arguments.\n");
+        return -1;
+    }
+    //Try to call the server three times.
+
     return 0;
 }
 
