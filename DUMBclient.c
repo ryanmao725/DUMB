4bf56cafd30b573f141ee0f54b78c46f41e45b92
ryanmao725 <ryan.mao.725@gmail.com>
2019-12-10 21:02:09 -0500

    Almost completed the terminal aspect of client

--- DUMBclient.c
+++ DUMBclient.c
@@ -3,12 +3,22 @@
 #define COMMAND_COUNT 7
 #define MAX_INPUT 256
 
+typedef struct command {
+    char* name;
+    char* server_name;
+    int key;
+    int arg_count;
+    char* messages[];
+} command;
+
+static command COMMAND_LIST[COMMAND_COUNT];
 static char* COMMANDS[COMMAND_COUNT] = {"quit","create","open","next","put","delete","close"};
+static char* SERVER_COMMANDS[COMMAND_COUNT] = {"GDBYE", "CREAT", "OPNBX", "NXTMG", "PUTMG", "DELBX", "CLSBX"};
 static int COMMAND_ARGS[COMMAND_COUNT] = {0, 1, 1, 0, 2, 1, 1};
 
 char* get_input(char* prompt) {
     char* input_buffer = malloc(sizeof(char) * MAX_INPUT);
-    printf("%s: ", prompt);
+    printf("%s> ", prompt);
     fgets(input_buffer, MAX_INPUT, stdin);
     input_buffer[strcspn(input_buffer, "\r\n")] = 0;
     return input_buffer;
@@ -36,22 +46,42 @@ int validate_command(char* input) {
     return i;
 }
 
+void request_args(int command_key, char* args[], int arg_number){
+    if (arg_number == 0) {
+        printf("Okay, which box name would you like to %s?\n", COMMANDS[command_key]);
+        args[arg_number] = get_input(COMMANDS[command_key]);
+    } else {
+        printf("Okay, what message would you like to %s?\n", COMMANDS[command_key]);
+        args[arg_number] = get_input(COMMANDS[command_key]);
+    }
+}
+
 int command_handler(int command_key) {
     int arg_count = 0;
     int num_args = COMMAND_ARGS[command_key];
-    //If the number of arguments is NOT 0, we want to initiate an array containing all arguments
+    //Retrieve the arguments
     char* args[num_args];
     while (arg_count < num_args) {
-        args[arg_count] = get_input("arg");
+        request_args(command_key, args, arg_count);
         arg_count++;
     }
+    //At this point, we have the arguments and the command, now we send this information to the server, and see what happens.
     return 0;
+}
+
+int init_client() {
+    //This method initializes all the commands and stores them into the array.
+    int i = 0;
+    for (i; i < COMMAND_COUNT; i++) {
+
+    }
+
 }
 
 int main(int argc, char** argv) {
     if (argc != 3) {
         printf("Error: Need to provide the IP-address and Port Number as arguments.\n");
-        return -1;
+        exit(0);
     }
     //Try to call the server three times.
     int connected = 1;
@@ -66,10 +96,9 @@ int main(int argc, char** argv) {
         printf("Unable to connect, exiting\n");
         exit(0);
     }
-    char* input_buffer = malloc(sizeof(char) * MAX_INPUT);
     while (connected) {
-        int command_key = validate_command(get_input("Input command"));
-        if (command_key == -1) continue;
+        int command_key = validate_command(get_input(">"));
+        if (command_key == -1) continue; //This means that the help command was called, continue to next command.
         if (command_key < 7) {
             //We know what command it is because the index is the command.
             //Call the command handler and pass in the index (command key).
