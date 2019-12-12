8707e049572f8cb68259fd15518f70006f383fd7
Shivan Modha <shivanmodha@gmail.com>
2019-12-11 21:54:45 -0500

    Client updates

--- DUMBclient.c
+++ DUMBclient.c
@@ -1,25 +1,4 @@
 #include "DUMBclient.h"
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-
-#define MAX_TRIES 3
-#define COMMAND_COUNT 7
-#define MAX_INPUT 256
-
-typedef struct command {
-    char* name;
-    char* server_name;
-    int key;
-    int arg_count;
-    char* messages[];
-} command;
-
-static unsigned short COMMAND_ARGS[COMMAND_COUNT] = {0, 1, 1, 0, 1, 1, 1};
-static char* COMMANDS[COMMAND_COUNT] = {"quit","create","open","next","put","delete","close"};
-static char* SERVER_COMMANDS[COMMAND_COUNT] = {"GDBYE", "CREAT", "OPNBX", "NXTMG", "PUTMG", "DELBX", "CLSBX"};
-static char* SERVER_RESPONSES[8] = {"OK!", "ER:WHAT?", "ER:EXIST", "ER:NEXST", "ER:OPEND", "ER:NOOPN", "ER:NOTMT", "ER:EMPTY"};
-static char* INTERPRET_RESPONSES[8] = {"Success!", "Your command is in some way broken or malformed.", "Box already exists.", "Box does not exist.", "Box is currently in use.", "Box is not open.", "Box is not empty.", "Box is empty."};
 
 char* get_input(char* prompt) {
     char* input_buffer = malloc(sizeof(char) * MAX_INPUT);
